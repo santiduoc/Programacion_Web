3197ca1e2ab40da1f2f48bbc4e58bc146daf6e59
santiduoc <101286302+santiduoc@users.noreply.github.com>
2022-06-29 17:08:52 -0400

    Add files via upload

--- ProyectosDjango/Totora/core/models.py
+++ ProyectosDjango/Totora/core/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+#Modelo para suscriptor
+class Suscriptor(models.Model):
+    rut = models.CharField(max_length=10,primary_key=True,verbose_name='Rut')
+    nombres = models.CharField(max_length=150,verbose_name='Nombres')
+    apellidos = models.CharField(max_length=150,verbose_name='Apellidos')
+    telefono = models.CharField(max_length=12,verbose_name='Telefono')
+    mail = models.CharField(max_length=30,verbose_name='E-Mail')
+    donacion = models.CharField(max_length=10,verbose_name='Monto Donacion')
+    
+    def __str__(self):
+        return self.rut
